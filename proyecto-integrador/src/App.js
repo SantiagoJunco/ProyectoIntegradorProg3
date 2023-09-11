600ebae9b6ee85d37b9cb89b1f7d45b734369fa0
luchosetton <lsetton@udesa.edu.ar>
2023-09-11 08:42:49 -0300

    avances

--- proyecto-integrador/src/App.js
+++ proyecto-integrador/src/App.js
@@ -4,6 +4,8 @@ import Footer from "./Components/Footer"
 import Home from './Screens/Home'
 import todasPopulares from "./Screens/todasPopulares"
 import todasCartel from "./Screens/todasCartel"
+import DetallePelicula from "./Screens/DetallePelicula"
+import SearchResults from './Screens/SearchResults'
 
 function App() {
   return (
@@ -11,9 +13,10 @@ function App() {
       <Header />
       <Switch>
         <Route path='/' exact={true} component={Home}  />
-        <Route path='/detalle/id/:id' component={Home} /> 
+        <Route path='/detalle/id/:id' component={DetallePelicula} /> 
         <Route path='/todasPopulares' component={todasPopulares} /> 
         <Route path='/todasCartel' component={todasCartel} /> 
+        <Route path='/busqueda/:busqueda' component={SearchResults} /> 
       </Switch>
       <Footer />
     </>
