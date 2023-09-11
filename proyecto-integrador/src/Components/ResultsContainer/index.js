df2f6874d709e3d97566a62ea533a645e16cd887
nazafloresm <nfloresmunoz@udesa.edu.ar>
2023-09-11 09:19:26 -0300

    Results container

---
+++ proyecto-integrador/src/Components/ResultsContainer/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import Resultado from "../Resultado"
+
+
+class ResultsContainer extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        console.log('Estas son las props del container', this.props);
+        return (
+            <section className="listado_detalle_generos-Favoritos-home-search" id="section">
+                {
+                    this.props.busqueda.length === 0 ?
+                        <h1>Trayendo peliculas...(puede ser que no haya resultados para tu búsqueda)</h1> :
+                        this.props.busqueda.map((peliculaBuscada) => <Resultado
+                        key={peliculaBuscada.id}
+                        id={peliculaBuscada.id}
+                        imagen={'https://image.tmdb.org/t/p/w500' + peliculaBuscada.poster_path}
+                        titulo={peliculaBuscada.title}
+                        descripcion={peliculaBuscada.overview}
+                    /> )
+                }
+            </section>
+        )
+    }
+}
+
+export default ResultsContainer
