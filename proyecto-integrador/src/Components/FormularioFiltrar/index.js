e04949d166d0711787c1728511f6458161b33402
nazafloresm <nfloresmunoz@udesa.edu.ar>
2023-09-11 09:10:28 -0300

    Formulario filtrar

---
+++ proyecto-integrador/src/Components/FormularioFiltrar/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export default class FormularioFiltrar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {valor: ''};
+      }
+    evitarSubmit(evento){
+        evento.preventDefault()
+        
+      }
+
+    controlarCambios(evento) {
+        this.setState({valor: evento.target.value},()=> this.props.filtrarPeliculas(this.state.valor));
+      }
+
+    render() {
+        return (
+            <form className="formulario" onSubmit={(evento)=> this.evitarSubmit(evento)}>
+                <input
+                    className="busqueda"
+                    placeholder="Búsqueda"
+                    name="busqueda"
+                    type="text" onChange={(event)=>this.controlarCambios(event)} value={this.state.valor} 
+                />
+                <button type="submit">Buscar</button>
+            </form>
+        )
+    }
+}
